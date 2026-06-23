74b124d0d0e599c2fecccf31c43db3c69b3f51e9
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-23 20:24:09 +0700

    creating a simple web server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// file system
-// 1 . read and write with "fs"
-
 const fs = require("fs");
+// new module
+const http = require("http");
+// ----------------------------------File-----------------
 // blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -9,19 +9,29 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output.txt", textOut);
 
 // non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (error, data1) => {
-	if (error) console.log("ERROR !");
+// fs.readFile("./txt/start.txt", "utf-8", (error, data1) => {
+// 	if (error) console.log("ERROR !");
 
-	console.log(data1);
-	fs.readFile(`./txt/${data1}.txt`, "utf-8", (error, data2) => {
-		console.log(data2);
-		fs.readFile("./txt/append.txt", "utf-8", (error, data3) => {
-			console.log(data3);
+// 	console.log(data1);
+// 	fs.readFile(`./txt/${data1}.txt`, "utf-8", (error, data2) => {
+// 		console.log(data2);
+// 		fs.readFile("./txt/append.txt", "utf-8", (error, data3) => {
+// 			console.log(data3);
 
-			fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-				console.log("your file has been written ^^");
-			});
-		});
-	});
+// 			fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+// 				console.log("your file has been written ^^");
+// 			});
+// 		});
+// 	});
+// });
+// console.log("Will read file");
+
+// --------------------------------Server---------------------
+// web server using node js
+const server = http.createServer((req, res) => {
+	res.end("hello from the server");
 });
-console.log("Will read file");
+// listen request on this URL "http://127.0.0.1:8000/"
+server.listen(8000, "127.0.0.1", () => {
+   console.log("Listen to requests on port 8000")
+});
