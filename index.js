93e16a13a2706b139e617df34feef81276c5a5ff
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-23 16:52:14 +0700

    read and write with fs -Async way-

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 // file system
 // 1 . read and write with "fs"
+
 const fs = require("fs");
+// blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `what is avocado ? ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (error, data1) => {
+	if (error) console.log("ERROR !");
 
-const textOut = `what is avocado ? ${textIn}.\nCreated on ${Date.now()}`;
+	console.log(data1);
+	fs.readFile(`./txt/${data1}.txt`, "utf-8", (error, data2) => {
+		console.log(data2);
+		fs.readFile("./txt/append.txt", "utf-8", (error, data3) => {
+			console.log(data3);
 
-fs.writeFileSync("./txt/output.txt", textOut);
-
-console.log("file written");
+			fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+				console.log("your file has been written ^^");
+			});
+		});
+	});
+});
+console.log("Will read file");
