1e2d94983a9039ac5c7f7e25c9759b61f87e9d72
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-24 16:34:03 +0700

    replace placeholder

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 // ----------------------------------File-----------------
+
 // blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -29,6 +30,19 @@ const url = require("url");
 
 // --------------------------------Server---------------------
 // b1 readFileSync
+const replaceTemplate = (temp, el) => {
+	// b2 replace all the placeholders
+	let output = temp.replace(/{%PRODUCTNAME%}/g, el.productName);
+	output = output.replace(/{%IMAGE%}/g, el.image);
+	output = output.replace(/{%PRICE%}/g, el.price);
+	output = output.replace(/{%FROM%}/g, el.from);
+	output = output.replace(/{%NUTRIENTS%}/g, el.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, el.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, el.description);
+	output = output.replace(/{%ID%}/g, el.id);
+	return output;
+};
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 // b1. read files
 const tempOverview = fs.readFileSync(
@@ -43,15 +57,23 @@ const tempProduct = fs.readFileSync(
 	`${__dirname}/templates/template-product.html`,
 	"utf-8",
 );
-const dataObject = JSON.parse(data);
 
+const dataObject = JSON.parse(data);
 
 // server function (run each time user request)
 const server = http.createServer((req, res) => {
 	const pathName = req.url; // before ? mark
 	// Overview page
 	if (pathName === "/" || pathName === "/overview") {
-		res.end("overview page");
+		res.writeHead(200, { "Content-type": "text/html" });
+
+		const cardsHtml = dataObject
+			.map((el) => replaceTemplate(tempCard, el))
+			.join("");
+		// b4 replace placeholder of this file
+		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+		res.end(output);
 
 		// Product page
 	} else if (pathName === "/product") {
