899e769e19da2dd4670f61128c3f77d5b2cdd7f2
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-25 08:11:45 +0700

    use replaceAll for better looking ^^

--- index.js
+++ index.js
@@ -30,21 +30,21 @@ const url = require("url");
 
 // --------------------------------Server---------------------
 // b1 readFileSync
-const replaceTemplate = (temp, el) => {
-	// b2 replace all the placeholders
-	let output = temp.replace(/{%PRODUCTNAME%}/g, el.productName);
-	output = output.replace(/{%IMAGE%}/g, el.image);
-	output = output.replace(/{%PRICE%}/g, el.price);
-	output = output.replace(/{%FROM%}/g, el.from);
-	output = output.replace(/{%NUTRIENTS%}/g, el.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, el.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, el.description);
-	output = output.replace(/{%ID%}/g, el.id);
+const replaceTemplate = (tempCard, el) => {
+	// b2: Replace all placeholders in the template-card  with actual product data
+	let output = tempCard.replace(/{%PRODUCTNAME%}/g, el.productName);
+	output = output.replaceAll("{%IMAGE%}", el.image);
+	output = output.replaceAll("{%PRICE%}", el.price);
+	output = output.replaceAll("{%FROM%}", el.from);
+	output = output.replaceAll("{%NUTRIENTS%}", el.nutrients);
+	output = output.replaceAll("{%QUANTITY%}", el.quantity);
+	output = output.replaceAll("{%DESCRIPTION%}", el.description);
+	output = output.replaceAll("{%ID%}", el.id);
 	return output;
 };
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-// b1. read files
+// b1: Read templates files synchronously
 const tempOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
 	"utf-8",
@@ -62,16 +62,17 @@ const dataObject = JSON.parse(data);
 
 // server function (run each time user request)
 const server = http.createServer((req, res) => {
-	const pathName = req.url; // before ? mark
+	const pathName = req.url; 
 	// Overview page
 	if (pathName === "/" || pathName === "/overview") {
 		res.writeHead(200, { "Content-type": "text/html" });
 
-		const cardsHtml = dataObject
+		const cardsContent = dataObject
+			// b3:mapping dataObject 
 			.map((el) => replaceTemplate(tempCard, el))
 			.join("");
-		// b4 replace placeholder of this file
-		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+		// b4 replace placeholder in tempOverview
+		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsContent);
 
 		res.end(output);
 
